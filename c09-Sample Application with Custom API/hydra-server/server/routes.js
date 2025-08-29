3e583f8171f57bbba173f8368991fee686600426
wilsonify <tom.andrew.wilson@gmail.com>
2025-08-29 18:00:04 -0400

    swagger ui at /api-explorer

--- c09-Sample Application with Custom API/hydra-server/server/routes.js
+++ c09-Sample Application with Custom API/hydra-server/server/routes.js
@@ -1,7 +1,16 @@
 import articlesRouter from './api/controllers/articles/router';
 import userRouter from './api/controllers/user/router';
+import path from 'path';
+import express from 'express';
 
 export default function routes(app) {
+  // API routes
   app.use('/api/v1/articles', articlesRouter);
   app.use('/api/v1/user', userRouter);
+
+  // Swagger UI docs (serving from /public/api-explorer)
+  app.use(
+    '/api-explorer',
+    express.static(path.join(process.cwd(), 'public', 'api-explorer'))
+  );
 }
